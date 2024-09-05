32859e455a225ecc27c70df9c17e8b0269550746
Rupert Foggo McKay <maxfmckay@gmail.com>
2024-09-05 20:22:35 +0200

    Internet connection logger

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type ConnectionEvent = {
+	type: "online" | "offline";
+	/**
+	 * Date.now() integer
+	 */
+	when: number;
+};
 
 export const App = () => {
-	const [count, setCount] = useState(0);
+	const [connectionEvents, setConnectionEvents] = useState<
+		Array<ConnectionEvent>
+	>(() => [
+		{ when: Date.now(), type: navigator.onLine ? "online" : "offline" },
+	]);
+
+	useEffect(() => {
+		const offlineHandler = () => {
+			setConnectionEvents((prev) => [
+				{ when: Date.now(), type: "offline" },
+				...prev,
+			]);
+		};
+		const onlineHandler = () => {
+			setConnectionEvents((prev) => [
+				{ when: Date.now(), type: "online" },
+				...prev,
+			]);
+		};
+		window.addEventListener("offline", offlineHandler);
+		window.addEventListener("online", onlineHandler);
+		return () => {
+			window.removeEventListener("offline", offlineHandler);
+			window.removeEventListener("online", onlineHandler);
+		};
+	}, []);
 
 	return (
 		<main className="stack center">
-			<h1>Hello world</h1>
-			<p>
-				This project is intended as a React/TypeScript starter template. You can
-				see the source here:{" "}
-				<a href="https://github.com/fildon/react-ts-starter/">
-					react-ts-starter
-				</a>
-			</p>
-			<span>
-				Click the button to prove React is working. If the number goes up, then
-				everything is good 👍
-			</span>
-			<button
-				onClick={() => {
-					setCount((c) => c + 1);
-				}}
-			>
-				{count}
-			</button>
+			<h1>Connection Status Logger</h1>
+			<h2>{`Current connection Status:`}</h2>
+			<p>{connectionEvents[0].type.toUpperCase()}</p>
+			<h2>Connection History</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Type</th>
+						<th>When</th>
+					</tr>
+				</thead>
+				<tbody>
+					{connectionEvents.map(({ when, type }) => (
+						<tr key={when}>
+							<td>{type}</td>
+							<td>{new Date(when).toString()}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</main>
 	);
 };
